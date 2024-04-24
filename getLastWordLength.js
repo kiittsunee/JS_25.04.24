a0f1458e5075c3493cfb2ef59f0cb9048317a5cf
ann <ocirovaanna5@gmail.com>
2024-04-24 23:45:18 +0800

    Функция getLastWordLength

---
+++ getLastWordLength.js
@@ -0,0 +1,10 @@
+const getLastWordLength = (str) => {
+    const string = str.trim().split(' ');
+    const lastWord = string[string.length - 1];
+    
+    return lastWord.length;
+  };
+  
+  console.log(getLastWordLength('')); // 0
+  console.log(getLastWordLength('moon is white')); // 5
+  console.log(getLastWordLength('Hello, World!  ')); // 6
